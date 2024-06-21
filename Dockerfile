1ce84ae0e93319b72a708dde557ae6e314466b41
gracetrianaa <gracetrianasurvinta@gmail.com>
2024-06-21 16:38:22 +0700

    feat: monitoring

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,14 @@ FROM python:3.8-slim
 # Set the working directory in the container
 WORKDIR /app
 
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    gcc \
+    libmariadb-dev \
+    libssl-dev \
+    pkg-config \
+    && rm -rf /var/lib/apt/lists/*
+
 # Copy the current directory contents into the container at /app
 COPY . /app
 
---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "google" {
+  project = "cloud-engineer-project-111" # Replace with your GCP project ID
+  region  = "us-west2"       # GCP region
+}
+
+resource "google_compute_instance" "app" {
+  name         = "budget-buddy-app"
+  machine_type = "e2-micro"
+  zone         = "us-west2-a" # Replace with a valid zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11" # Replace with a valid image if needed
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  tags = ["http-server"]
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+    sudo systemctl start docker
+    USERNAME=$(whoami)
+    sudo usermod -aG docker $USERNAME
+    sudo -u $USERNAME docker run -d -p 80:5000 gracetriana/income-expense-app
+  EOF
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  target_tags = ["http-server"]
+}
